4417a86f034440137624d12de22a97ef926e5c4e
Anthony Lopez <anthony.lopez@unmsm.edu.pe>
2017-11-20 10:34:39 -0500

    video 31 Controlador de Vistas ok

---
+++ vistas/header.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>zooFISI</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <link rel="shortcut icon" href="./public/imagenes/favicon.png" type="image/png">
+  <link rel="stylesheet" href="./public/css/responsimple.min.css">
+  <link rel="stylesheet" href="./public/css/zoo_fisi.css">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb"
+    crossorigin="anonymous">
+</head>
+
+<body>
+
+  <header class="container  center  header">
+      <div class="item  i-b  v-middle  ph12  lg2">
+        <h1 class="logo">zooFISI</h1>
+      </div>
+      <nav class="item  i-b  v-middle  ph12  lg10  menu">
+        <ul class="container">
+          <li class="nobullet  item  inline"><a href="./">Inicio</a></li>
+          <li class="nobullet  item  inline"><a href="animales">Animales</a></li>
+          <li class="nobullet  item  inline"><a href="trabajadores">Trabajadores</a></li>
+          <li class="nobullet  item  inline"><a href="itinerario">Itinerario</a></li>
+          <li class="nobullet  item  inline"><a href="habitad">Hábitad</a></li>
+          <li class="nobullet  item  inline"><a href="zona">Zona</a></li>
+          <li class="nobullet  item  inline"><a href="salir">Salir</a></li>
+        </ul>
+      </nav>
+  </header>
+
+  <main class="container  center  main">
